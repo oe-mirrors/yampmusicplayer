a5534f0c131567ffca0bc7dab3d45dc02eb00bca
Captain <openatv@gmail.com>
2024-05-25 12:01:24 +0200

    update setup.py add missing packages=[],

--- setup.py
+++ setup.py
@@ -7,4 +7,6 @@ pkg = 'Extensions.YampMusicPlayer'
 setup(name='enigma2-plugin-extensions-yampmusicplayer',
 	description='Music Player with Artist-Art Background',
 	cmdclass=setup_translate.cmdclass,
+	# explicitly declare packages so setuptools does not attempt auto discovery
+	packages=[],
 )
